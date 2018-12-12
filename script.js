f086563c9da957c534b081da6d030400dd0243fa
Issac Teoh <42070824+bluetimer@users.noreply.github.com>
2018-12-12 17:30:06 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,13 @@
+$(".magic1").click(function() {
+    $(".magic1pic").slideToggle();
+});
+
+$(".magic2").click(function() {
+    $(".magic2pic").toggle();
+    
+    
+});
+
+$(".magic3").click(function() {
+    $(".magic3pic").fadeToggle();
+});
